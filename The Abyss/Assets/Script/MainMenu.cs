98f0dad4f302a61d4e95249e41a924c3b414a62d
Xiiraito <159627415+Xiiraito@users.noreply.github.com>
2024-02-18 18:12:12 +0700

    mainmenu v.0.1

---
+++ The Abyss/Assets/Script/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LaodLevel()
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex)
+    }
+
+    IEnumerator LoadAsynchronously ()]
+    {
+        
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
