c03c3937010ec03a9b6ddb2edbe9f8baf4eb68b0
chetsadaphon24 <114966348+chetsadaphon24@users.noreply.github.com>
2024-02-19 04:07:18 +0700

    pushssssss

--- The Abyss/Assets/Script/pushobjects.cs
+++ The Abyss/Assets/Script/pushobjects.cs
@@ -7,11 +7,23 @@ public class pushobjects : MonoBehaviour
 {
     public float distance = 1f;
     public LayerMask RockMask;
+    private string currentState;
+    private Animator animator;
+    private bool isGrounded;
+
+    private string Idle = "idie";
+    private string Walk = "walk";
+    private string Run = "run";
+    private string Crouch_idle = "Crouch idle";
+    private string Crouch_Walk = "Crouch Walk";
+    private string Jump = "Jump";
+    private string Pushs = "pushs";
+    private string Pushs_Idle = "push_Idle";
 
     GameObject rock;
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -19,19 +31,35 @@ public class pushobjects : MonoBehaviour
     {
         Physics2D.queriesStartInColliders = false;
         RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right * transform.localScale.x, distance, RockMask);
-        if (hit.collider != null && Input.GetKey(KeyCode.E))
+        if (hit.collider != null && hit.collider.gameObject.tag == "pushable" && Input.GetKey(KeyCode.E))
         {
             rock = hit.collider.gameObject;
 
             rock.GetComponent<FixedJoint2D>().enabled = true;
             rock.GetComponent<FixedJoint2D>().connectedBody = this.GetComponent<Rigidbody2D>();
-            print("yes");
+            isGrounded = true;
+            ChangeAnimationState(Pushs_Idle);
         }
+        else if (Input.GetKeyUp(KeyCode.E))
+        {
+            rock.GetComponent<FixedJoint2D>().enabled = false;
+            ChangeAnimationState(Idle);
+        }
+
+
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawLine(transform.position, (Vector2)transform.position + Vector2.right * transform.localScale.x * distance);
+    }
+    void ChangeAnimationState(string newState)
+    {
+        if (currentState == newState) return;
+
+        animator.Play(newState);
+
+        currentState = newState;
     }
 }
