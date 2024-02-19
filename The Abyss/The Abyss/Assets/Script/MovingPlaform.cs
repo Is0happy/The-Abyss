d6fb3d54fb868aa6666e2e197c1b7a195cbd2996
Xiiraito <159627415+Xiiraito@users.noreply.github.com>
2024-02-19 11:20:07 +0700

    Move platform

---
+++ The Abyss/The Abyss/Assets/Script/MovingPlaform.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
+using UnityEngine;
+
+public class MovingPlaform : MonoBehaviour
+{
+    public Transform posA, posB;
+
+    public float speed;
+
+    Vector3 targetPos;
+
+    bool isTouch;
+
+    private void Start()
+    {
+        targetPos = posA.position;
+        isTouch = false;
+    }
+
+    private void Update()
+    {
+
+        if (isTouch == true)
+        {
+            targetPos = posB.position;
+        }
+        if (isTouch == false) 
+        {
+            targetPos = posA.position;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isTouch = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isTouch = false;
+        }
+    }
+}
